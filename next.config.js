e453bc1bbbc6b9b5ae7320326748ffbcbfe1d73b
naohiko560 <66761935+naohiko560@users.noreply.github.com>
2023-04-22 22:30:09 +0900

    next.config.jsのベースパス表記変更

--- next.config.js
+++ next.config.js
@@ -2,13 +2,16 @@
 
 const branchName = process.env.BRANCH_NAME ? '/' + process.env.BRANCH_NAME : '';
 
+const urlPrefix = '/NextjsMarkdownBlog';
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['localhost'],
   },
   assetPrefix: branchName,
-  basePath: branchName,
+  basePath: urlPrefix,
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
